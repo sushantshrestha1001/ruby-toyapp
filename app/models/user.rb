9b0d071620dc0388e0e7373a410a9a7a4d1c0545
Sushant <sushantshrestha1001@gmail.com>
2019-01-14 19:17:34 +0545

    Adding microposts and finishing toy app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :microposts
+    
+  validates :name, length: { maximum: 10 }, presence: true    # Replace FILL_IN with the right code.
+  validates :email, length: { maximum: 10 }, presence: true    # Replace FILL_IN with the right code.
+
 end
